cb6d16da983b777aa7026b6924433414b5d0b6a4
Sergey Seitov <sseitov@neosphere.com>
2014-12-31 02:01:53 +0300

    add dropbox

--- app/src/main/java/com/vchannel/glucograph/MainActivity.java
+++ app/src/main/java/com/vchannel/glucograph/MainActivity.java
@@ -1,23 +1,40 @@
 package com.vchannel.glucograph;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.CalendarView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.Locale;
 
+import com.dropbox.sync.android.DbxAccountManager;
+import com.dropbox.sync.android.DbxFile;
+import com.dropbox.sync.android.DbxFileInfo;
+import com.dropbox.sync.android.DbxFileSystem;
+import com.dropbox.sync.android.DbxPath;
 
 public class MainActivity extends Activity {
 
-    TextView currentDateText;
-    CalendarView calendar;
-    BloodValue currentValue;
+    private static final String appKey = "9yjxpsg3ck2b19d";
+    private static final String appSecret = "ma6d3huqfcbpkbk";
+
+    private static final int REQUEST_LINK_TO_DBX = 0;
+
+    private TextView currentDateText;
+    private CalendarView calendar;
+    private BloodValue currentValue;
+
+    private DbxAccountManager dbxAccountManager;
 
     void setCurrentDate(Date date) {
         DateFormat df = DateFormat.getDateInstance(DateFormat.MEDIUM, Locale.getDefault());
@@ -29,6 +46,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dbxAccountManager = DbxAccountManager.getInstance(getApplicationContext(), appKey, appSecret);
 
         currentDateText = (TextView)findViewById(R.id.currentDate);
 
@@ -65,9 +84,72 @@ public class MainActivity extends Activity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            if (dbxAccountManager.hasLinkedAccount()) {
+                doSynchro();
+            } else {
+                dbxAccountManager.startLink((Activity)this, REQUEST_LINK_TO_DBX);
+            }
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_LINK_TO_DBX) {
+            if (resultCode == Activity.RESULT_OK) {
+                doSynchro();
+            } else {
+                Toast t = Toast.makeText(this, "Link to Dropbox failed or was cancelled.", Toast.LENGTH_LONG);
+                t.show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    void doSynchro() {
+        try {
+            final String DB_FILE_NAME = "Glucograph2.sqlite";
+            DbxPath dbxPath = new DbxPath(DbxPath.ROOT, DB_FILE_NAME);
+
+            DbxFileSystem dbxFs = DbxFileSystem.forAccount(dbxAccountManager.getLinkedAccount());
+
+            if (!dbxFs.exists(dbxPath)) {
+                DbxFile testFile = dbxFs.create(dbxPath);
+                try {
+                    testFile.writeString("Hello Dropbox");
+                } finally {
+                    testFile.close();
+                }
+                return;
+            }
+
+            if (dbxFs.isFile(dbxPath)) {
+                String resultData;
+                DbxFile dbxFile = dbxFs.open(dbxPath);
+                try {
+                    FileInputStream inputStream = dbxFile.getReadStream();
+                    byte[] mBuffer = new byte[1024];
+                    int mLength;
+                    int allBytes = 0;
+                    while ((mLength = inputStream.read(mBuffer))>0)
+                    {
+                        allBytes += mLength;
+//                        mOutput.write(mBuffer, 0, mLength);
+                    }
+//                    mOutput.flush();
+//                    mOutput.close();
+                    inputStream.close();
+
+                    Toast t = Toast.makeText(this, "Do Dropbox synchro "+allBytes, Toast.LENGTH_LONG);
+                    t.show();
+                } finally {
+                    dbxFile.close();
+                }
+            }
+        } catch (IOException e) {
+        }
     }
 }
